e433ea24f4c5563fa9debd1e404c89b838942e69
Vipin Nair <vipinsthename@gmail.com>
2015-05-26 13:24:52 +0100

    Changes

--- src/test/scala/default/RecordedSimulation.scala
+++ src/test/scala/default/RecordedSimulation.scala
@@ -1,4 +1,4 @@
-package com.aowp.nodeapptest
+package nodeAppTests
 
 import scala.concurrent.duration._
 
@@ -9,39 +9,63 @@ import io.gatling.jdbc.Predef._
 class RecordedSimulation extends Simulation {
 
 	val httpProtocol = http
-		.baseURL("http://nodeapp.54.76.215.147.xip.io/")
-		//.inferHtmlResources(BlackList(""".*\.css""", """.*\.ico"""), WhiteList())
-		.acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+		.baseURL("http://nodeapp.54.77.223.221.xip.io")
+		.inferHtmlResources()
+		.acceptHeader("application/json, text/javascript, */*; q=0.01")
 		.acceptEncodingHeader("gzip, deflate")
 		.acceptLanguageHeader("en-GB,en;q=0.5")
 		.connection("keep-alive")
 		.userAgentHeader("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:37.0) Gecko/20100101 Firefox/37.0")
 
-	val headers_3 = Map("Accept" -> "*/*")
+	val headers_0 = Map("Accept" -> "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
 
-	val headers_4 = Map(
-		"Accept" -> "application/json, text/javascript, */*; q=0.01",
-		"X-Requested-With" -> "XMLHttpRequest")
+	val headers_3 = Map("Accept" -> "application/font-woff;q=0.9,*/*;q=0.8")
 
-    val uri1 = "http://nodeapp.54.76.215.147.xip.io/"
+	val headers_4 = Map("Accept" -> "*/*")
+
+	val headers_5 = Map("X-Requested-With" -> "XMLHttpRequest")
+
+	val headers_8 = Map("Accept" -> "image/png,image/*;q=0.8,*/*;q=0.5")
+
+	val headers_9 = Map(
+		"Accept" -> "application/font-woff;q=0.9,*/*;q=0.8",
+		"Accept-Encoding" -> "identity")
+
+    val uri1 = "http://nodeapp.54.77.223.221.xip.io"
+    val uri2 = "self-repair.mozilla.org"
 
 	val scn = scenario("RecordedSimulation")
 		.exec(http("request_0")
 			.get("/")
+			.headers(headers_0)
 			.resources(http("request_1")
-			.get(uri1 + "/favicon.ico?_=1"),
+			.get("http://" + uri2 + "/en-GB/repair")
+			.headers(headers_0),
             http("request_2")
-			.get(uri1 + "/favicon.ico?_=1"),
+			.get(uri1 + "/favicon.ico?_=1")
+			.headers(headers_0),
             http("request_3")
+			.get(uri1 + "/fonts/RotisSansSerifExtraBold75.ttf")
+			.headers(headers_3),
+            http("request_4")
 			.get(uri1 + "/scripts/main.js")
-			.headers(headers_3)))
-		.pause(3)
-		.exec(http("request_4")
-			.get("/api/questions")
-			.headers(headers_4)
-			.resources(http("request_5")
-			.get(uri1 + "/api/questions")
 			.headers(headers_4)))
+		.pause(4)
+		.exec(http("request_5")
+			.get("/api/ratings")
+			.headers(headers_5)
+			.resources(http("request_6")
+			.get(uri1 + "/api/questions")
+			.headers(headers_5),
+            http("request_7")
+			.get(uri1 + "/api/questions")
+			.headers(headers_5),
+            http("request_8")
+			.get(uri1 + "/img/stars.png")
+			.headers(headers_8),
+            http("request_9")
+			.get(uri1 + "/fonts/fontawesome-webfont.woff?v=4.3.0")
+			.headers(headers_9)))
 
-	setUp(scn.inject(atOnceUsers(200))).protocols(httpProtocol)
+	setUp(scn.inject(atOnceUsers(50))).protocols(httpProtocol)
 }
